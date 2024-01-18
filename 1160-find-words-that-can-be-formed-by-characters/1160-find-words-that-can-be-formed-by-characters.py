a0ca42bb803c8698dac8eca853e7d5330baf5e2c
Manjunath Raju S G <132266851+ManjunathrajuSG@users.noreply.github.com>
2024-01-18 22:26:26 +0530

    Time: 182 ms (33.27%), Space: 14.1 MB (75.80%) - LeetHub

---
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def countCharacters(self, words, chars):
+        """
+        :type words: List[str]
+        :type chars: str
+        :rtype: int
+        """
+        char_count = {}
+        result = 0
+
+        # Count characters in chars
+        for char in chars:
+            char_count[char] = char_count.get(char, 0) + 1
+
+        # Check each word in words
+        for word in words:
+            word_count = {}
+            is_good_word = True
+
+            # Count characters in the current word
+            for char in word:
+                word_count[char] = word_count.get(char, 0) + 1
+
+            # Check if the word can be formed using chars
+            for char, count in word_count.items():
+                if char not in char_count or count > char_count[char]:
+                    is_good_word = False
+                    break
+
+            # If the word is good, add its length to the result
+            if is_good_word:
+                result += len(word)
+
+        return result
+
+solution_instance = Solution()
+print(solution_instance.countCharacters(["cat","bt","hat","tree"], "atach"))  
+print(solution_instance.countCharacters(["hello","world","leetcode"], "welldonehoneyr"))  # 
